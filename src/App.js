62f3df908f70efd08e334fc0d888c15c5c180edc
Normalyk <anatoliy.chkhailo@gmail.com>
2020-12-19 18:46:57 +0200

    User

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, {Component} from 'react';
+import UserMain from "./Component/user/userMain";
 
 class App extends Component {
-
+     users = [{ name: 'vasya', age: 31, status: false, skills: ['java', 'js'] },
+        { name: 'petya', age: 30, status: true, skills: ['java', 'js', 'html'] },
+        { name: 'kolya', age: 29, status: true, skills: ['mysql', ',mongo'] },
+        { name: 'olya', age: 28, status: false, skills: ['java', 'js'] },
+        { name: 'max', age: 30, status: true, skills: ['mysql', ',mongo'] }];
     render() {
         return (
             <div>
+                {this.users.map(value => {
+                return(<UserMain item = {value}/>)
 
+                })
+                }
             </div>
         );
     }
