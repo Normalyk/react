aa20a37f34d49e9c075e99c7bbd479a32e60ef48
Normalyk <anatoliy.chkhailo@gmail.com>
2020-12-24 19:13:47 +0200

    dz_4_posts done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React, {Component} from 'react';
+import AllUsers from './components/all-users/AllUsers';
+import AllPosts from './components/all-posts/AllPosts';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+
 
 class App extends Component {
 
-    render() {
-        return (
-            <div>
 
-            </div>
+    render() {
+
+        return (
+            <Router>
+                <div>
+                    <div>
+                        <Link to={'/users'}>
+                            users
+                        </Link>
+                    </div>
+
+                    <div>
+                        <Link to={'/posts'}>
+                            posts
+                        </Link>
+                    </div>
+
+                    <hr/>
+                    <Switch>
+                        <Route path={'/users'} render={() => {
+                            return <AllUsers/>;
+                        }}/>
+                        <Route path={'/posts'} render={() => {
+                            return <AllPosts/>;
+                        }}/>
+                    </Switch>
+                    <hr/>
+
+
+                </div>
+            </Router>
         );
     }
 }
---
+++ src/components/all-posts/AllPosts.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {PostService} from '../services/PostService';
+import Post from "../post/Post";
+import './AllPosts.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+
+class AllPosts extends Component {
+    state = {posts: []};
+    postService = new PostService();
+
+    async componentDidMount() {
+
+        let posts = await this.postService.getAllPosts();
+        this.setState({posts});
+
+    }
+
+    render() {
+        let {posts } = this.state;
+        return (
+            <div>
+                {posts .map(value => <Post key={value.id} item={value}/>)}
+
+                <div className={'nest'}>
+
+                    <Switch>
+                        <Route path={'/posts/:id'} render={(props) => {
+                            return 'salo';
+
+                        }}/>
+
+                    </Switch>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default AllPosts;
